8841162fbcaebad28c623b1675e4f262fc1db317
Anton jubin <103031115+thomas7272@users.noreply.github.com>
2024-06-18 23:46:28 +0530

    Maker sharks Assessment

---
+++ demo/src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.User;
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<String> registerUser(@RequestBody User user) {
+        userService.registerUser(user);
+        return ResponseEntity.ok("User registered successfully");
+    }
+
+    @GetMapping("/fetch")
+    public ResponseEntity<User> fetchUser(@RequestParam String username) {
+        return userService.fetchUserByUsername(username)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+}
